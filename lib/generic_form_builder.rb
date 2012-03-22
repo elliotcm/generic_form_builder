6e44282af9f70bd8a1fc1d1ffe5e3610c207fb0f
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2012-03-22 11:18:07 +0000

    Account for more complicated radio values.

--- lib/generic_form_builder.rb
+++ lib/generic_form_builder.rb
@@ -42,7 +42,7 @@ class GenericFormBuilder < ActionView::Helpers::FormBuilder
     return super if options[:default_builder]
     label_text = options.delete(:label) || field.to_s.humanize
     options[:tag_type] ||= :li
-    content_tag(options[:tag_type], label("#{field}_#{value}", super + " #{label_text} #{errors_text(field)}"))
+    content_tag(options[:tag_type], label("#{field}_#{value.to_s.downcase.gsub(' ', '_')}", super + " #{label_text} #{errors_text(field)}"))
   end
 
   def buttons(options = {})
